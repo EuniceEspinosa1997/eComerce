b367b3fb6ae41c384ff82554452c7dc9b5d1b82b
Eunice Espinosa Arreola <43525342+EuniceEspinosa1997@users.noreply.github.com>
2021-08-04 20:47:39 -0500

    FixingBugs
    rename of files in CapitalCase
    prettier code

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import { useProducts } from '../../Hooks/useProducts';            
-import ProductItem from './ProductItem';
+import React from "react";
+import { useProducts } from "../../Hooks/useProducts";
+import ProductItem from "./ProductItem";
 
 function ProductsList() {
-    const { products } = useProducts();
-    
-    return (
-     <>
-      <h1 className = 'title'>Productos</h1>
-        <div className = 'products'>
-            {
-                
-                products.map(product =>(
-                 <ProductItem 
-                    product = {product}
-                    key = {product.id + "003"}
-                 />
-                ))
-                
-            }
-        </div>
-     </>
-    )
+  const { products } = useProducts();
+
+  return (
+    <>
+      <h1 className="title">Productos</h1>
+      <div className="products">
+        {products.map(product => (
+          <ProductItem product={product} key={product.id + "003"} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default ProductsList;
--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import ProductsList from './Products/ProductsList';
-import Details from './Details/Details'
-import Index from './Index/Index';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ProductsList from "./Products/ProductsList";
+import Details from "./Details/Details";
+import Index from "./Index/Index";
 
 function Routing() {
-    return (
-        <section>
-           <Switch>
-               <Route path = '/' exact component = {Index}/>
-               <Route path = '/products' exact component = {ProductsList}/>
-               <Route path = '/details' exact component = {Details}/>
-            </Switch> 
-        </section>
-    )
+  return (
+    <section>
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/products" exact component={ProductsList} />
+        <Route path="/details" exact component={Details} />
+      </Switch>
+    </section>
+  );
 }
 
 export default Routing;
